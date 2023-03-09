189ede06f17f2015e77dba0132518e5951efbda6
Iruneguinea29 <iruneguinea29@gmail.com>
2023-03-09 10:21:35 +0100

    trying to auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '../styles/globals.css';
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-  }
-  
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App

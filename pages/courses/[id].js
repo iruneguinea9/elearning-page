fdf22eecdbd59dcdd24eaea0932f9b443a1a7dae
Iruneguinea29 <iruneguinea29@gmail.com>
2023-03-13 13:44:04 +0100

    trying to redirect

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -1,57 +1,32 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-import utilStyles from '../../styles/utils.module.css';
-import Format from '../../layout/format';
-import fetcher from '../../lib/fetcher';
+import { useRouter } from 'next/router'
 import { parseCookies } from 'nookies';
+import fetcher from '../../lib/fetcher';
 
-export default function Course({ postData }) {
-  const router = useRouter();
+export default function Course({ course }) {
+  const router = useRouter()
 
   if (router.isFallback) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
-  if (!postData) {
-    return <div>No data found</div>;
-  }
+
   return (
-    <Format>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <div className="container md:px-20 ">
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <h2 className={utilStyles.headingMd}>{postData.description}</h2>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </div>
-      </article>
-    </Format>
-  );
+    <div>
+      <h1>{course.title}</h1>
+      <p>{course.description}</p>
+    </div>
+  )
 }
 
-export async function getServerSideProps(context) {
-  const { params, req, res } = context;
-  const { id } = params;
-
-  const cookies = parseCookies({ req });
+export async function getServerSideProps({ params }) {
+  const { courseId } = params
+  const cookies = parseCookies();
   const accessToken = cookies.token;
+  // Retrieve the course data from the API using the courseId parameter
+  const data = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}/courses/${courseId}`, accessToken)
 
-  try {
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/courses/${id}`;
-    console.log('fetching data from', url);
-    console.log('accessToken', accessToken);
-    const postData = await fetcher(url, accessToken, { timeout: context.serverRuntimeConfig.timeout });
-    console.log('postData', postData);
-    return {
-      props: {
-        postData,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    res.writeHead(302, { Location: '/authenticatedindex' });
-    res.end();
+  return {
+    props: {
+      course: data
+    }
   }
 }

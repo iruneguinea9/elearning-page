4acd6f0462772fef00bcdabbf8cb04cf33de92d6
Iruneguinea29 <iruneguinea29@gmail.com>
2023-03-14 13:37:06 +0100

    it fetches!

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -8,9 +8,9 @@ export default function CoursePage() {
   const { id } = router.query;
   const cookies = parseCookies();
 
-  const { data: course, error } = useSWR(`/courses/${id}`, (url) => fetcher(url, cookies.token));
+  const { data: course, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/courses/${id}`, (url) => fetcher(url, cookies.token));
 
-  if (error) return <div>Error loading course data.</div>;
+  if (error) return <div>Error loading course data.</div>; 
   if (!course) return <div>Loading course data...</div>;
 
   return (

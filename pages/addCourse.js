a8b32be514ca8240ee7a98d3ba01de9ef325e0e2
Iruneguinea29 <iruneguinea29@gmail.com>
2023-03-16 12:21:39 +0100

    trying to add lessons

--- pages/addCourse.js
+++ pages/addCourse.js
@@ -9,12 +9,24 @@ function AddCourse() {
   const [formData, setFormData] = useState({
     title: '',
     description: '',
-    disabled: false, // initialize disabled to false
+    lessons: [], // initialize lessons to an empty array
+    disabled: false,
   });
 
   const handleChange = (e) => {
-    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value; // update value of disabled field based on checkbox value
-    setFormData({ ...formData, [e.target.name]: value });
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    if (e.target.name === 'lessons') {
+      // update lessons array
+      const lessons = formData.lessons.map((lesson, index) => {
+        if (index === parseInt(e.target.dataset.index)) {
+          return { ...lesson, [e.target.dataset.field]: value };
+        }
+        return lesson;
+      });
+      setFormData({ ...formData, lessons });
+    } else {
+      setFormData({ ...formData, [e.target.name]: value });
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -29,7 +41,7 @@ function AddCourse() {
     const data = await response.json();
     console.log(data);
     // Reset form data
-    setFormData({ title: '', description: '', disabled: false }); // reset disabled to false
+    setFormData({ title: '', description: '', lessons: [], disabled: false });
   };
   const cookies = parseCookies();
   const accessToken = cookies.token;
@@ -60,7 +72,21 @@ function AddCourse() {
                         Disabled:
                         <input type="checkbox" name="disabled" checked={formData.disabled} onChange={handleChange} />
                     </label>
-                    <button  className={styles.lessonButton} type="submit">Add Course</button>
+                    <h2>Lessons:</h2>
+                    {formData.lessons.map((lesson, index) => (
+                      <div key={index}>
+                        <label>
+                          Title:
+                          <input type="text" name="title" value={lesson.title} data-index={index} data-field="title" onChange={handleChange} />
+                        </label>
+                        <label>
+                          Content:
+                          <input type="text" name="content" value={lesson.content} data-index={index} data-field="content" onChange={handleChange} />
+                        </label>
+                      </div>
+                    ))}
+                    <button className={styles.lessonButton} onClick={() => setFormData({ ...formData, lessons: [...formData.lessons, { title: '', content: '' }] })}>Add Lesson</button>
+                    <button className={styles.lessonButton} type="submit">Add Course</button>
                 </div>  
             </form> 
         </Format>

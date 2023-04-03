37720290b83d9f76e601a87efb28bcfbe4e49276
Irune <iruneguinea29@gmail.com>
2023-04-03 11:36:09 +0200

    fixes

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,23 @@
 // Name : Footer
 // Author : Irune Guinea
 // This component is to mantain a uniform format in all the pages
-// Last update 03/04/2023 - V2
+// Last update 03/04/2023 - V3
 
 export default function footer() {
 
-  const bg = {
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: "bottom left"
-  }
   // ########################################## RETURN ##########################################
   return (
-    <footer className="bg-blue-400" style={bg}>
+    <footer>
+        <div class="skew skew-top mr-for-radius">
+        <svg class="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+          <polygon fill="currentColor" points="0 0 10 10 0 10"></polygon>
+        </svg>
+      </div>
+      <div class="skew skew-top ml-for-radius">
+        <svg class="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+          <polygon fill="currentColor" points="0 10 10 0 10 10"></polygon>
+        </svg>
+      </div>
       <div class="py-20 bg-gray-900 radius-for-skewed">
         <div class="max-w-md mx-auto text-center">
           <p class="inline-block mb-6 mx-auto text-white text-3xl font-bold leading-none">
